e9a1c4091096b64d008070f8bea8af6c89a6ba17
Amaury Demilie <ade@escaux.com>
2023-09-27 10:06:47 +0200

    update latest

---
+++ lotto.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="lotto" tilewidth="32" tileheight="32" tilecount="100" columns="10">
+ <image source="dstny_trooper_asset/lotto-dstny-logo-pixel.png" width="320" height="320"/>
+</tileset>
